8c23568fbfed2502809210f3ecbe79ab2eccd090
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-05-04 21:59:50 +0200

    finish styling all sections to mobile view

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ const portfolio = [
     img: "panel.jpg",
     tech: ["php", "mysql", "html", "css"],
     description: `This system is for a manufacturing company.
-      It started with a customer base and simple orders, and for over 7 years of adding new functionalities, the system has:
+      Project is started with jutr two tables in data base: customer and orders. For over 7 years of adding new functionalities, the system has grow to:
       <ul>
-      <li>- warehouse, register of purchases and suppliers</li>
-      <li>- offers for customers sent by e-mail</li>
-      <li>- register of customers and orders (adding, editing and deleting)
-      <li>- issuing invoices for orders + generating PDF with sending to e-mail</li>
-      <li>- implementation of the tasks of production employees</li>
-      <li>- transport orders</li>
-      <li>- detailed various data analysis reports</li>
-      <li>- valuations for clients</li>
-      <li>- daily reports</li>
-      <li>- managing users and a number of different settings</li>
-      <li>- separate login panel for drivers and customers.
+        <li>- warehouse, register of purchases and suppliers</li>
+        <li>- offers for customers sent by e-mail</li>
+        <li>- register of customers and orders (adding, editing and deleting)</li>
+        <li>- issuing invoices for orders + generating PDF with sending to e-mail</li>
+        <li>- implementation of the tasks of production employees</li>
+        <li>- transport orders </li>
+        <li>- detailed various data analysis reports</li>
+        <li>- valuations for clients</li>
+        <li>- daily reports</li>
+        <li>- managing users and a number of different settings</li>
+        <li>- separate login panel for drivers and customers.</li>
       </ul>
-      Everything is programmed as agreed with the client.`,
+      Everything is programmed as agreed with the client wish.`,
   },
   {
     title: "Lego Star Wars Minifigure Collection",
@@ -39,7 +39,7 @@ const portfolio = [
     img: "figures.jpg",
     tech: ["react", "redux", "js"],
     description:
-      "I'm a huge fan and collector of Lego Star Wars minifigures. When my collection had about 80 figures I started to get a bit lost, I needed a list, I wanted something better than excel. Being a programmer, I wrote a web page with a database of my figures. This application is the second version, mainly written for learning JS and React. At the moment the database is in a JSON file, when I finish the Node.JS course I will be able to finish this application.",
+      "I'm a huge fan and collector of Lego Star Wars minifigures. When my collection had about 80 figures I started to get a bit lost, I needed a better list (with images), something better than excel. Being a programmer, I wrote a web page with a database of my figures. For this day i use this web page. This application is the next version, mainly written for learning JS and React. At the moment the database is in a JSON file, when I finish the Node.JS course I will be able to finish this application.",
   },
 
   {
@@ -107,3 +107,5 @@ portfolio.map(item => {
 
   portfolioList.insertAdjacentHTML("beforeEnd", html);
 });
+
+
