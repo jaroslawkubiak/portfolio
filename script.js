a34e8c3af5303d07ded169601abfc8c8ad7af554
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-05-05 17:57:27 +0200

    add slider effect in portfolio preview images

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-import { svg, svgDemo, svgGithub } from "./svg.js";
+import { svg, svgDemo, svgGithub, arrowRight, arrowLeft } from "./svg.js";
 
 const burgerMenu = document.getElementById("burger");
 const mobileMenu = document.getElementById("mobile-menu");
@@ -9,11 +9,38 @@ burgerMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
 
+// menu smooth navigation
+document.getElementById("mobile-menu").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//menu fade animation
+const nav = document.querySelector(".nav");
+const hendleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav-link");
+
+    siblings.forEach(el => {
+      const btn = el.classList.contains("nav-link--btn");
+      if (el !== link && !btn) el.style.opacity = this;
+    });
+  }
+};
+nav.addEventListener("mouseover", hendleHover.bind(0.3));
+nav.addEventListener("mouseout", hendleHover.bind(1));
+
+// portfolio array
 const portfolio = [
   {
     title: "ERP System",
     link: "portfolio/panel",
-    img: "panel.jpg",
+    imgDir: "panel",
+    images: 4,
     tech: ["php", "mysql", "html", "css"],
     description: `This system is for a manufacturing company.
       Project is started with jutr two tables in data base: customer and orders. For over 7 years of adding new functionalities, the system has grow to:
@@ -36,7 +63,8 @@ const portfolio = [
     title: "Lego Star Wars Minifigure Collection",
     link: "portfolio/figures",
     github: "https://github.com/jaroslawkubiak/figures",
-    img: "figures.jpg",
+    imgDir: "figures",
+    images: 3,
     tech: ["react", "redux", "js"],
     description:
       "I'm a huge fan and collector of Lego Star Wars minifigures. When my collection had about 80 figures I started to get a bit lost, I needed a better list (with images), something better than excel. Being a programmer, I wrote a web page with a database of my figures. For this day i use this web page. This application is the next version, mainly written for learning JS and React. At the moment the database is in a JSON file, when I finish the Node.JS course I will be able to finish this application.",
@@ -46,7 +74,8 @@ const portfolio = [
     title: "Flag game",
     link: "portfolio/flags",
     github: "https://github.com/jaroslawkubiak/flag-game",
-    img: "flag.jpg",
+    imgDir: "flag",
+    images: 2,
     tech: ["js", "html", "css"],
     description:
       "Flag game Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu velit egestas, ultrices nisl vel, porta ipsum. Maecenas maximus felis a efficitur maximus. Sed elementum nisl lectus, a molestie tellus hendrerit in. Duis egestas velit et metus scelerisque, at placerat ipsum luctus. Duis auctor auctor ex, vel fringilla est pharetra a.",
@@ -55,7 +84,8 @@ const portfolio = [
     title: "Snake game",
     link: "portfolio/snake",
     github: "https://github.com/jaroslawkubiak/snake",
-    img: "snake.jpg",
+    imgDir: "snake",
+    images: 1,
     tech: ["js", "html", "css"],
     description:
       " Snake game Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu velit egestas, ultrices nisl vel, porta ipsum. Maecenas maximus felis a efficitur maximus. Sed elementum nisl lectus, a molestie tellus hendrerit in. Duis egestas velit et metus scelerisque, at placerat ipsum luctus. Duis auctor auctor ex, vel fringilla est pharetra a.",
@@ -64,7 +94,8 @@ const portfolio = [
     title: "Pig game",
     link: "portfolio/pig",
     github: "https://github.com/jaroslawkubiak/pig-game",
-    img: "pig.jpg",
+    imgDir: "pig",
+    images: 1,
     tech: ["js", "html", "css"],
     description:
       "Pig game Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu velit egestas, ultrices nisl vel, porta ipsum. Maecenas maximus felis a efficitur maximus. Sed elementum nisl lectus, a molestie tellus hendrerit in. Duis egestas velit et metus scelerisque, at placerat ipsum luctus. Duis auctor auctor ex, vel fringilla est pharetra a.",
@@ -74,13 +105,15 @@ const portfolio = [
     title: "F1 - reflex game",
     link: "portfolio/f1",
     github: "https://github.com/jaroslawkubiak/reflex",
-    img: "f1.jpg",
+    imgDir: "f1",
+    images: 1,
     tech: ["js", "html", "css"],
     description:
       "Inspired by one of the videos on Instagram where F1 driver Lando Norris tests his reflexes. I want to have a similar app, so in two afternoons I wrote it. At the start of the race, the best F1 drivers have a score of around 0.15s from turning off the lights to pressing the throttle. This app measures your reflexes with an accuracy of 0.0001 seconds. See how fast you are. ",
   },
 ];
 
+// map through portfolio array to create html
 portfolio.map(item => {
   let html = `
     <li class="portfolio-item-wrapper">
@@ -95,24 +128,95 @@ portfolio.map(item => {
 
   html += `</div><div class="portfolio-description">${item.description}</div></div>`;
 
-  html += `<div class="portfolio-preview">`;
-  html += `<img src="/portfolio/preview/${item.img}" class="portfolio-preview-image" alt="Preview" /></div>`;
+  html += `<div class="portfolio-preview" id="${item.imgDir}">`;
+
+  for (let i = 1; i <= item.images; i++) {
+    // console.log(`/portfolio/preview/${item.imgDir}/${i}.jpg`);
+    html += `<div class="slide"><img src="/portfolio/preview/${item.imgDir}/${i}.jpg" alt="Preview photo ${i}" class="portfolio-preview-image" /></div>`;
+  }
+
+  html += `<button class="slider-btn slider-btn-left">${arrowLeft}</button>
+  <button class="slider-btn slider-btn-right">${arrowRight}</button>
+  <div class="dots"></div>`;
+  html += `</div>`;
 
   html += `<div class="portfolio-footer"><div class="portfolio-footer-github">`;
   if (item.github)
     html += `<a href="${item.github}" target="_blank"><span>${svgGithub}</span> <span>GitHub</span></a>`;
-  html += `</div><div class="portfolio-footer-demo"><a href="${item.link}" target="_blank"><span>${svgDemo}</span> <span>Live demo</span></a></div></div>`;
+  html += `</div><div class="portfolio-footer-demo"><a href="${item.link}" target="_blank"><span>${svgDemo}</span> <span>Live demo</span></a></div>`;
 
   html += `</div></li>`;
 
   portfolioList.insertAdjacentHTML("beforeEnd", html);
-});
 
-// menu smooth navigation
-document.getElementById("mobile-menu").addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains('nav-link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  }
+  // slider effect
+  const containerId = document.getElementById(item.imgDir);
+  const slides = containerId.querySelectorAll(".slide");
+  const btnLeft = containerId.querySelector(".slider-btn-left");
+  const btnRight = containerId.querySelector(".slider-btn-right");
+  const dotContainer = containerId.querySelector(".dots");
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // creating dots under slider
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots-dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  // select active dot
+  const activeDot = function (slide) {
+    containerId
+      .querySelectorAll(".dots-dot")
+      .forEach(dot => dot.classList.remove("dots-dot-active"));
+    containerId
+      .querySelector(`.dots-dot[data-slide="${slide}"]`)
+      .classList.add("dots-dot-active");
+  };
+
+  // changing slides
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // first page load
+  const init = function () {
+    createDots();
+    goToSlide(0);
+    activeDot(0);
+  };
+  init();
+
+  // next slide
+  const nextSlide = function () {
+    curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+  // previous slide
+  const previousSlide = function () {
+    curSlide === 0 ? (curSlide = maxSlide - 1) : curSlide--;
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+  // event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", previousSlide);
+
+  // event listener for dots
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots-dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activeDot(slide);
+    }
+  });
 });
